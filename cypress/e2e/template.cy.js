53794fe07281cb4f5385b536c022953a459e8595
Huambo87 <maximgladkov@hotmail.com>
2023-12-02 16:32:45 -0500

    Changes to be committed: 	modified:   cypress/e2e/template.cy.js

--- cypress/e2e/template.cy.js
+++ cypress/e2e/template.cy.js
@@ -1,8 +1,8 @@
 ///<reference types = "Cypress"/>
 
-// describe("spec template", () => {
-//     it('test1', () => {
-//         cy.visit("https://tmk-dev2.dev2.webant.ru/passport/login/oauth/authorize?client_id=26f537e02a3015e918cc&response_type=code&redirect_uri=https%3A%2F%2Ftmk.dev2.webant.ru&scope=read&state=0o0zbf9y11ne")
-//         cy.url().should("eq", "https://tmk-dev2.dev2.webant.ru/passport/login/oauth/authorize?client_id=26f537e02a3015e918cc&response_type=code&redirect_uri=https%3A%2F%2Ftmk.dev2.webant.ru&scope=read&state=0o0zbf9y11ne")
-//     })
-// })
+describe("spec template", () => {
+    it('test1', () => {
+        cy.visit("https://tmk-dev2.dev2.webant.ru/passport/login/oauth/authorize?client_id=26f537e02a3015e918cc&response_type=code&redirect_uri=https%3A%2F%2Ftmk.dev2.webant.ru&scope=read&state=0o0zbf9y11ne")
+        cy.url().should("eq", "https://tmk-dev2.dev2.webant.ru/passport/login/oauth/authorize?client_id=26f537e02a3015e918cc&response_type=code&redirect_uri=https%3A%2F%2Ftmk.dev2.webant.ru&scope=read&state=0o0zbf9y11ne")
+    })
+})
