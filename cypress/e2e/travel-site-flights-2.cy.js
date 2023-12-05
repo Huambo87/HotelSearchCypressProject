3969a664ceb7a048778748d88a014554163da02e
Huambo87 <maximgladkov@hotmail.com>
2023-12-04 20:18:30 -0500

    modified:   cypress/e2e/travel-site-flights-2.cy.js

--- cypress/e2e/travel-site-flights-2.cy.js
+++ cypress/e2e/travel-site-flights-2.cy.js
@@ -6,6 +6,12 @@ describe('Flights Tab Test to Check Flights Functionality', () => {
     // const origin_placeholder = '[placeholder = "City or airport"][name = "origin"]'
     const origin_placeholder = '[name = "origin"]'
     const destination_placeholder = '[name = "destination"]'
+    const checkin = '#input-start-2'
+    const checkout = '#input-end-2'
+    const first_class_rbutton = '[id = "f_class"]'
+    const business_rbutton = '[id = "business"]'
+    const economy_rbutton = '[id = "economy"]'
+
 
     before(() => {
         cy.visit("http://localhost:3000")
@@ -23,23 +29,29 @@ describe('Flights Tab Test to Check Flights Functionality', () => {
         cy.log("after each")
     })    
 
-
     it('Navigate to Flight Tab', () => {
         cy.get(origin_placeholder).should("be.visible")
+        cy.get(destination_placeholder).should("be.visible")
         cy.get(flights_tab).invoke('attr', 'aria-expanded').should('eq', 'true')
         cy.get(flights_tab).invoke('text').should('eq', 'flight')
-        
-
     })
 
-        
     it('Populate Origin and Destination', () => {
         cy.get(origin_placeholder).type("Toronto").invoke('val').should('eq', 'Toronto')
         cy.get(destination_placeholder).type("Baku").invoke('val').should('eq', 'Baku')
-
-        
-
     })
 
+    it('Populate Check-in/out', () => {
+        cy.get(checkin).type('31/12/2023').invoke('val').should('eq', '31/12/2023')
+        cy.get(checkout).type('06/01/2024').invoke('val').should('eq', '06/01/2024')
+        cy.get(checkout).clear()
+        cy.get(checkout).type('25/01/2024').invoke('val').should('eq', '25/01/2024')
+    })
+
+    it('Check Radiobuttons', () => {
+        cy.get(first_class_rbutton).click().should('be.checked')
+        cy.get(business_rbutton).click().should('be.checked')
+        cy.get(economy_rbutton).click().should('be.checked')
+    })
 
 })
