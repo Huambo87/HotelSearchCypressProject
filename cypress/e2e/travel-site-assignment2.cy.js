c00eba00c9a08b4f652314098141381d7a822765
Huambo87 <maximgladkov@hotmail.com>
2023-12-07 18:54:56 -0500

    Changes to be committed: 	modified:   cypress/e2e/travel-site-assignment2.cy.js

--- cypress/e2e/travel-site-assignment2.cy.js
+++ cypress/e2e/travel-site-assignment2.cy.js
@@ -1,11 +1,12 @@
 ///<reference types = "Cypress"/>
 
-describe('Car Tab Test to Check Flights Functionality', () => {
+describe('Car Tab Test to Check Car Functionality', () => {
 
     const car_tab = '[data-cy="Car"]'
     const location_placeholder = '[name = "location"]'
     const dropoff_placeholder = '[name = "dropoff"]'
     const dropoff = '[data-drop = "2"]'
+    const car_group_list = '[name = "car-group"]'
 
     before(() => {
         cy.visit("http://localhost:3000")
@@ -29,6 +30,19 @@ describe('Car Tab Test to Check Flights Functionality', () => {
     it('Populate Dropoff', () => {
         cy.get(dropoff).clear()
         cy.get(dropoff).type('01/01/2024').invoke('val').should('eq', '01/01/2024')
+    })
+
+    it('Select Car Group', () => {
+        cy.get(car_group_list).should("be.visible")
+        cy.get(car_group_list).select('Group 1', {force: true})
+        cy.get('[title = "Group 1"]').should("be.visible")
+
+        cy.get(car_group_list).select('Group 2', {force: true})
+        cy.get('[title = "Group 2"]').should("be.visible")
+
+        cy.get(car_group_list).select('Group 3', {force: true})
+        cy.get('[title = "Group 3"]').should("be.visible")
+        
     })
 
 })
