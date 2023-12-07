4bce5e18ae0fdc2a48bb4d8b5028166307ba3c78
Huambo87 <maximgladkov@hotmail.com>
2023-12-07 17:55:51 -0500

    Changes to be committed: 	modified:   cypress/e2e/template.cy.js 	modified:   cypress/e2e/travel-site-assignment2.cy.js

--- cypress/e2e/template.cy.js
+++ cypress/e2e/template.cy.js
@@ -1,5 +1,7 @@
 ///<reference types = "Cypress"/>
 
+const { describe } = require("mocha")
+
 describe("spec template", () => {
     it('test1', () => {
         cy.visit("https://tmk-dev2.dev2.webant.ru/passport/login/oauth/authorize?client_id=26f537e02a3015e918cc&response_type=code&redirect_uri=https%3A%2F%2Ftmk.dev2.webant.ru&scope=read&state=0o0zbf9y11ne")
